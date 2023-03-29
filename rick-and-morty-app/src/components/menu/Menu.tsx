1d3162dc1e71afa0851da6a0dd279afb784ebec4
Estevão Gomes <estevaog.gomes@gmail.com>
2023-03-29 13:10:59 +0000

    Revert "[Feature"
    Commit message with error.
    
    This reverts commit b134fee0478d1ffac0b0a4f51f93887650fc41a8.

--- rick-and-morty-app/src/components/menu/Menu.tsx
+++ rick-and-morty-app/src/components/menu/Menu.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
 import styles from "./Menu.module.css";
 
 export function Menu() {
-  const [showMenu, setShowMenu] = useState(true);
-  
-  function handleClick(){
-    setShowMenu(showMenu => !showMenu)
-  }
-  
-  return (
-    <div className={showMenu ? styles.menu : styles.menuHidden}>
-      Menu
-      <button onClick={handleClick}>button</button>
-    </div>
-  );
+  return <div className={styles.menu}>Menu</div>;
 }
