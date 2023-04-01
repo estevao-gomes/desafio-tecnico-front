03f8084e895ee4b31df78a6b52f4d3f4746330e2
Estevão <estevaog.gomes@gmail.com>
2023-04-01 16:36:49 -0300

    [Bugfix]Fix scrollbar on small screens for menu

--- rick-and-morty-app/src/components/menu/Menu.tsx
+++ rick-and-morty-app/src/components/menu/Menu.tsx
@@ -75,108 +75,106 @@ export function Menu() {
 
   return (
     <div className={styles.menu}>
-      <div className={`${showMenu && styles.menuHidden}`}>
-        <div>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <h2 className={styles.title}>Filtros</h2>
-            <label className={styles.label} htmlFor="Name">
-              Nome
+      <div className={`${showMenu ? styles.menuNoButton : styles.menuHidden}`}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h2 className={styles.title}>Filtros</h2>
+          <label className={styles.label} htmlFor="Name">
+            Nome
+          </label>
+          <input
+            name="Name"
+            className={styles.input}
+            value={formValues.name}
+            onChange={(event) =>
+              setFormValues({ ...formValues, name: event.target.value })
+            }
+            aria-label="Nome"
+            placeholder="Nome"
+          />
+          <fieldset className={styles.checkFilter}>
+            <legend>Status</legend>
+            <label htmlFor="status-option1">
+              Alive
+              <input
+                type="checkbox"
+                id="status-option1"
+                name="alive"
+                checked={formValues.status.alive}
+                onChange={handleStatusCheckboxChange}
+              />
             </label>
-            <input
-              name="Name"
-              className={styles.input}
-              value={formValues.name}
-              onChange={(event) =>
-                setFormValues({ ...formValues, name: event.target.value })
-              }
-              aria-label="Nome"
-              placeholder="Nome"
-            />
-            <fieldset className={styles.checkFilter}>
-              <legend>Status</legend>
-              <label htmlFor="status-option1">
-                Alive
-                <input
-                  type="checkbox"
-                  id="status-option1"
-                  name="alive"
-                  checked={formValues.status.alive}
-                  onChange={handleStatusCheckboxChange}
-                />
-              </label>
-              <label htmlFor="status-option2">
-                Dead
-                <input
-                  type="checkbox"
-                  id="status-option2"
-                  name="dead"
-                  checked={formValues.status.dead}
-                  onChange={handleStatusCheckboxChange}
-                />
-              </label>
-              <label htmlFor="status-option3">
-                Unknown
-                <input
-                  type="checkbox"
-                  id="status-option3"
-                  name="unknown"
-                  checked={formValues.status.unknown}
-                  onChange={handleStatusCheckboxChange}
-                />
-              </label>
-            </fieldset>
-            <fieldset className={styles.checkFilter}>
-              <legend>Gender</legend>
-              <label htmlFor="gender-option1">
-                Male
-                <input
-                  type="checkbox"
-                  id="gender-option1"
-                  name="male"
-                  checked={formValues.gender.male}
-                  onChange={handleGenderCheckboxChange}
-                />
-              </label>
-              <label htmlFor="gender-option3">
-                Genderless
-                <input
-                  type="checkbox"
-                  id="gender-option3"
-                  name="genderless"
-                  checked={formValues.gender.genderless}
-                  onChange={handleGenderCheckboxChange}
-                />
-              </label>
-              <label htmlFor="gender-option2">
-                Female
-                <input
-                  type="checkbox"
-                  id="gender-option2"
-                  name="female"
-                  checked={formValues.gender.female}
-                  onChange={handleGenderCheckboxChange}
-                />
-              </label>
-              <label htmlFor="gender-option4">
-                Unknown
-                <input
-                  type="checkbox"
-                  id="gender-option4"
-                  name="unknown"
-                  checked={formValues.gender.unknown}
-                  onChange={handleGenderCheckboxChange}
-                />
-              </label>
-            </fieldset>
-            <button
-              aria-label="Filtrar"
-              className={styles.submitButton}
-              type="submit"
-            >
-              Filtrar
-            </button>
-          </form>
-        </div>
+            <label htmlFor="status-option2">
+              Dead
+              <input
+                type="checkbox"
+                id="status-option2"
+                name="dead"
+                checked={formValues.status.dead}
+                onChange={handleStatusCheckboxChange}
+              />
+            </label>
+            <label htmlFor="status-option3">
+              Unknown
+              <input
+                type="checkbox"
+                id="status-option3"
+                name="unknown"
+                checked={formValues.status.unknown}
+                onChange={handleStatusCheckboxChange}
+              />
+            </label>
+          </fieldset>
+          <fieldset className={styles.checkFilter}>
+            <legend>Gender</legend>
+            <label htmlFor="gender-option1">
+              Male
+              <input
+                type="checkbox"
+                id="gender-option1"
+                name="male"
+                checked={formValues.gender.male}
+                onChange={handleGenderCheckboxChange}
+              />
+            </label>
+            <label htmlFor="gender-option3">
+              Genderless
+              <input
+                type="checkbox"
+                id="gender-option3"
+                name="genderless"
+                checked={formValues.gender.genderless}
+                onChange={handleGenderCheckboxChange}
+              />
+            </label>
+            <label htmlFor="gender-option2">
+              Female
+              <input
+                type="checkbox"
+                id="gender-option2"
+                name="female"
+                checked={formValues.gender.female}
+                onChange={handleGenderCheckboxChange}
+              />
+            </label>
+            <label htmlFor="gender-option4">
+              Unknown
+              <input
+                type="checkbox"
+                id="gender-option4"
+                name="unknown"
+                checked={formValues.gender.unknown}
+                onChange={handleGenderCheckboxChange}
+              />
+            </label>
+          </fieldset>
+          <button
+            aria-label="Filtrar"
+            className={styles.submitButton}
+            type="submit"
+          >
+            Filtrar
+          </button>
+        </form>
         <div className={styles.options}>
           <h2 className={styles.title}>opções</h2>
           <label className={styles.label}>
