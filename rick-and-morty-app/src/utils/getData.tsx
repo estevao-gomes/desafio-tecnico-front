a4d0be7ec7ba177e38af53b9622e6f02e1e51af1
Estevão <estevaog.gomes@gmail.com>
2023-04-01 12:02:19 -0300

    [Refactor]Fix usage of reqct-query to eliminate state
    Eliminates states to save data from main cards page, also changes pagination to use a state, saving current page

--- rick-and-morty-app/src/utils/getData.tsx
+++ rick-and-morty-app/src/utils/getData.tsx
@@ -10,7 +10,11 @@ import { ReactNode } from "react";
 export const queryClient = new QueryClient();
 
 //obtem dado da APi utilizando filtros, pela função "getData"
-export const useGetCharacters = (filters: { name?: string; id?: string }) => {
+export const useGetCharacters = (filters: {
+  name?: string;
+  id?: string;
+  page: number;
+}) => {
   return useQuery({
     queryKey: ["data", filters],
     queryFn: async () => {
@@ -27,19 +31,20 @@ export function CharacterQuery({ children }: { children: ReactNode }) {
   );
 }
 
-export async function getData(
-  filter?: { name?: string; id?: string },
-  url: string = "https://rickandmortyapi.com/api/character"
-) {
-  //Checa se a url é para a pagina inicial, ou uma pagina subsequente, pois se for para pagina subsequente o /? no filtro joga para a incial
-  const res =
-    url === "https://rickandmortyapi.com/api/character"
-      ? filter?.id
-        ? await axios.get(`${url}/${filter.id}`)
-        : await axios.get(
-            `${url}/?${filter?.name ? `name=${filter.name}` : ""}`
-          )
-      : await axios.get(`${url}&${filter?.name ? `name=${filter.name}` : ""}`);
+export async function getData(filter?: {
+  name?: string;
+  id?: string;
+  page: number;
+}) {
+  const url = "https://rickandmortyapi.com/api/character";
+  //Checa se a url é para a pagina inicial, ou pagina de personagem específico (se há id setado no filtro)
+  const res = filter?.id
+    ? await axios.get(`${url}/${filter.id}`)
+    : await axios.get(
+        `${url}/?page=${filter?.page}${
+          filter?.name ? `&name=${filter.name}` : ""
+        }`
+      );
 
   if (res.status === 404) {
     throw new Error("No results found");
