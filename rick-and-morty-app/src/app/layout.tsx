6a96225e17b442ff25d2b30d19b6c04a2d280d21
Estevão <estevaog.gomes@gmail.com>
2023-04-01 16:18:47 -0300

    [Feature]add filter for favorites only

--- rick-and-morty-app/src/app/layout.tsx
+++ rick-and-morty-app/src/app/layout.tsx
@@ -1,21 +1,29 @@
-import { Menu } from "@/components/menu/Menu";
-import { Navbar } from "@/components/navbar/Navbar";
-import { FilterContext } from "@/contexts/filterContext";
+import { Roboto } from "next/font/google"
 
-import "./globals.css";
+import { Menu } from "@/components/menu/Menu"
+import { Navbar } from "@/components/navbar/Navbar"
+import { FilterContext } from "@/contexts/filterContext"
+
+import "./globals.css"
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export const metadata = {
   title: "Rick and Morty Characters",
   description: "Rick and Morty interactive characters list",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>
         <Navbar />
         <FilterContext>
@@ -26,5 +34,5 @@ export default function RootLayout({
         </FilterContext>
       </body>
     </html>
-  );
+  )
 }
